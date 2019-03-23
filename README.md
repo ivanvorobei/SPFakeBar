d82fcdef6deec027a8d24c8689508be4be8c2ca9
Ivan Vorobei <ivanvorobei@icloud.com>
2019-03-23 19:25:06 +0300

    add files

---
+++ README.md
@@ -0,0 +1,76 @@
+#SPFakeBar
+
+It pod allow simple added navigation bar and full customize it - height or added custom views.
+
+Preview GIF is loading `3mb`. Please, wait.
+
+<img src="https://github.com/IvanVorobei/SPFakeBar/blob/master/Resources/Preview.gif" width="500">
+
+You can download example [from AppStore](https://itunes.apple.com/app/id1446635818) or see [video preview](https://xcode-shop.com/assets/preview/debts.mov). If you want to buy source code of the full app, please go to [xcode-shop.com](https://xcode-shop.com). Price: $200.
+
+<img src="https://github.com/IvanVorobei/SPStorkController/blob/master/Resources/Shop.svg"/>
+
+I have a store where I sell applications and modules for Xcode projects. You can find source codes of applications or custom animations / UI. I regularly update the code. Visit my website to see all items for sale: [xcode-shop.com](https://xcode-shop.com). On the website you can find previews and for some items links to AppStore.
+
+<img src="https://github.com/IvanVorobei/SPStorkController/blob/master/Resources/Shop.svg"/>
+
+## Requirements
+Swift 4.2. Ready for use on iOS 10+
+
+## Integration
+Put `Source/SPStorkController` folder in your Xcode project. Make sure to enable `Copy items if needed` and `Create groups`.
+
+Or via CocoaPods:
+```ruby
+pod 'SPFakeBar'
+```
+
+## How to use
+
+You may want to add a navigation bar to your modal controller. Since it became impossible to change or customize the native controller in swift 4 (I couldn’t even find a way to change the height of the bar), I had to recreate navigation bar from the ground up. Visually it looks real, but it doesn’t execute the necessary functions:
+
+```swift
+import UIKit
+
+class ModalController: UIViewController {
+    
+    let navBar = SPFakeBarView(style: .stork)
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+
+        self.navBar.titleLabel.text = "Title"
+        self.navBar.leftButton.setTitle("Cancel", for: .normal)
+        self.navBar.leftButton.addTarget(self, action: #selector(self.dismissAction), for: .touchUpInside)
+
+        self.view.addSubview(self.navBar)
+    }
+}
+```
+
+You only need to add a navigation bar to the main view, it will automatically layout. Use style `.stork` in init of `SPFakeBarView`. Here is visual preview with Navigation Bar and without it:
+
+<img src="https://github.com/IvanVorobei/SPStorkController/blob/master/Resources/Navigation%20Bar.jpg"/>
+
+## My projects
+
+Here I would like to offer you my other projects.
+
+### SPStorkController
+I created [SPStorkController](https://github.com/IvanVorobei/SPStorkController). It is a modal controller like in Mail or Apple Music application. Similar animation and transition. You can see an example of using controller in app [in AppStore](https://itunes.apple.com/app/id1446635818).
+
+<img src="https://github.com/IvanVorobei/SPStorkController/blob/master/Resources/Preview.gif" width="500">
+
+### SPPermission
+Project [SPPermission](https://github.com/IvanVorobei/SPPermission) for managing permissions with customizable visual effects. Beautiful dialog increases the chance of approval (which is important when we request notification). Simple control of this module saves you hours of development. You can start using project with just two lines of code and easy customization!
+
+<img src="https://github.com/IvanVorobei/SPPermission/blob/master/Resources/Preview.gif" width="500">
+
+## License
+`SPStorkController` is released under the MIT license. Check `LICENSE.md` for details.
+
+## Contact
+If you need any application or UI to be developed, message me at hello@ivanvorobei.by. I develop iOS apps and create designs, too. I use `swift` for development. To request more functionality, you should create a new issue. 
+Here are my apps in AppStore: [first account](https://itunes.apple.com/us/developer/polina-zubarik/id1434528595) & [second account](https://itunes.apple.com/us/developer/mikalai-varabei/id1435792103).
---
+++ Source/SPFakeBar/SPFakeBarView.swift
@@ -0,0 +1,245 @@
+// The MIT License (MIT)
+// Copyright © 2017 Ivan Vorobei (hello@ivanvorobei.by)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import UIKit
+
+open class SPFakeBarView: UIView {
+    
+    public var style: SPFakeBarNavigationStyle = .small {
+        didSet {
+            self.updateStyle()
+        }
+    }
+    
+    private var settedHeight: CGFloat = 0
+    public var height: CGFloat {
+        get {
+            return (self.settedHeight) + (self.addStatusBarHeight ? UIViewController.statusBarHeight : 0)
+        }
+        set {
+            self.settedHeight = newValue
+            self.updateHeight()
+        }
+        
+    }
+    
+    public var addStatusBarHeight: Bool = true {
+        didSet {
+            self.updateHeight()
+        }
+    }
+    
+    public var elementsColor: UIColor = SPFakeBarView.navigationElementsColor {
+        didSet {
+            self.leftButton.setTitleColor(self.elementsColor, for: .normal)
+            self.leftButton.setTitleColor(self.elementsColor.withAlphaComponent(0.7), for: .highlighted)
+            self.rightButton.setTitleColor(self.elementsColor, for: .normal)
+            self.rightButton.setTitleColor(self.elementsColor.withAlphaComponent(0.7), for: .highlighted)
+        }
+    }
+    
+    var closeButtonPossition: CloseButtonPosition = .none {
+        didSet {
+            self.leftButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            self.rightButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            switch self.closeButtonPossition {
+            case .left:
+                self.leftButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+            case .right:
+                self.rightButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+            case .none:
+                break
+            }
+        }
+    }
+    
+    var titleLabel = UILabel.init()
+    var subtitleLabel = UILabel.init()
+    var leftButton = UIButton.init()
+    var rightButton = UIButton.init()
+    
+    private let blurView = UIVisualEffectView.init(style: .extraLight)
+    private let separatorView = UIView()
+    
+    private var titleBottomConstraint: NSLayoutConstraint?
+    private var heightConstraint: NSLayoutConstraint?
+    private var topConstraint: NSLayoutConstraint?
+    private var leadingConstraint: NSLayoutConstraint?
+    private var trailingConstraint: NSLayoutConstraint?
+    
+    init(style: SPFakeBarNavigationStyle) {
+        super.init(frame: CGRect.zero)
+        self.style = style
+        self.commonInit()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.style = .small
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(self.blurView)
+        self.blurView.translatesAutoresizingMaskIntoConstraints = false
+        self.blurView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        self.blurView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        self.blurView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        self.blurView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        self.addSubview(self.separatorView)
+        self.separatorView.backgroundColor = UIColor.init(hex: "BFBFBF")
+        self.separatorView.translatesAutoresizingMaskIntoConstraints = false
+        self.separatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        self.separatorView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        self.separatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        self.separatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        
+        self.addSubview(self.titleLabel)
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
+        self.titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
+        self.titleBottomConstraint = self.titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12)
+        self.titleBottomConstraint?.isActive = true
+        
+        self.addSubview(self.subtitleLabel)
+        self.subtitleLabel.textColor = UIColor.init(hex: "8E8E92")
+        self.subtitleLabel.font = UIFont.system(weight: .demiBold, size: 13)
+        self.subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.subtitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
+        self.subtitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
+        self.subtitleLabel.bottomAnchor.constraint(equalTo: self.titleLabel.topAnchor, constant: 0).isActive = true
+        
+        self.leftButton.setTitleColor(self.elementsColor)
+        self.leftButton.titleLabel?.textAlignment = .left
+        self.leftButton.titleLabel?.font = UIFont.system(weight: .demiBold, size: 16)
+        self.leftButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 17, bottom: 0, right: 0)
+        self.addSubview(self.leftButton)
+        self.leftButton.translatesAutoresizingMaskIntoConstraints = false
+        self.leftButton.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        self.leftButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12).isActive = true
+        
+        self.rightButton.setTitleColor(self.elementsColor)
+        self.rightButton.titleLabel?.textAlignment = .right
+        self.rightButton.titleLabel?.font = UIFont.system(weight: .demiBold, size: 17)
+        self.rightButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 16)
+        self.addSubview(self.rightButton)
+        self.rightButton.translatesAutoresizingMaskIntoConstraints = false
+        self.rightButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        self.rightButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12).isActive = true
+        
+        self.closeButtonPossition = .none
+        
+        self.setContraints()
+        self.updateStyle()
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        self.setContraints()
+    }
+    
+    private func setContraints() {
+        if let superview = self.superview {
+            if self.topConstraint == nil {
+                
+                self.topConstraint = self.topAnchor.constraint(equalTo: superview.topAnchor)
+                self.topConstraint?.isActive = true
+                self.leadingConstraint = self.leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+                self.leadingConstraint?.isActive = true
+                self.trailingConstraint = self.trailingAnchor.constraint(equalTo: superview.trailingAnchor)
+                self.trailingConstraint?.isActive = true
+                
+                self.heightConstraint = self.heightAnchor.constraint(equalToConstant: self.height)
+                self.heightConstraint?.isActive = true
+                self.updateHeight()
+            }
+        }
+    }
+    
+    private func updateStyle() {
+        switch self.style {
+        case .small:
+            if UIViewController.statusBarHeight == 44 {
+                self.height = 88 - 44
+                self.titleBottomConstraint?.constant = -12
+            } else {
+                self.height = 64 - 20
+                self.titleBottomConstraint?.constant = -12
+            }
+            self.addStatusBarHeight = true
+            self.titleLabel.font = UIFont.system(weight: .demiBold, size: 17)
+            self.titleLabel.setCenterAlignment()
+        case .stork:
+            self.height = 66
+            self.titleBottomConstraint?.constant = -12
+            self.addStatusBarHeight = false
+            self.titleLabel.font = UIFont.system(weight: .demiBold, size: 17)
+            self.titleLabel.setCenterAlignment()
+        case .large:
+            if UIViewController.statusBarHeight == 44 {
+                self.height = 140 - 44
+                self.titleBottomConstraint?.constant = -8
+            } else {
+                self.height = 112 - 20
+                self.titleBottomConstraint?.constant = -4
+            }
+            self.addStatusBarHeight = true
+            self.titleLabel.font = UIFont.system(weight: .bold, size: 34)
+            self.titleLabel.textAlignment = .left
+            break
+        case .noContent:
+            self.height = 0
+            self.addStatusBarHeight = true
+        }
+        
+        self.updateConstraints()
+    }
+    
+    private func updateHeight() {
+        self.heightConstraint?.constant = self.height
+        self.updateConstraints()
+    }
+    
+    enum CloseButtonPosition {
+        case left
+        case right
+        case none
+    }
+}
+
+extension SPFakeBarView {
+    
+    static var navigationElementsColor: UIColor {
+        get {
+            if UINavigationBar.appearance().tintColor != nil {
+                return UINavigationBar.appearance().tintColor
+            } else {
+                return SPNativeColors.blue
+            }
+        }
+        set {
+            UINavigationBar.appearance().tintColor = newValue
+        }
+    }
+}
